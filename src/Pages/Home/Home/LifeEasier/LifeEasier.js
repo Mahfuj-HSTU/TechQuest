b74514494546cd68e8d310be33b89d389513caa7
Mahfuj-HSTU <rmahfujur16@gmail.com>
2023-01-11 00:19:46 +0600

    setup

---
+++ src/Pages/Home/Home/LifeEasier/LifeEasier.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LifeEasier = () => {
+    return (
+        <div>
+            <h2>We make life easier for 600+ companies since 2015</h2>
+        </div>
+    );
+};
+
+export default LifeEasier;
