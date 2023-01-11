a868d697fcc55106358a8a68a8bf7a779d6fbce1
amJayem <csejayem19@gmail.com>
2023-01-11 22:26:04 +0600

    latest news added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
             <Banner></Banner>
             <LookingJobs></LookingJobs>
             <LifeEasier />
-            <Events></Events>
+            <Events></Events> 
             <LatestNews/>
             <WeAreSpecialist />
         </div>
