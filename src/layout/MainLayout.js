7ad2f87ce2d53f0af5cfbb70de5bf408b64dc72d
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2023-01-11 19:27:01 +0600

    added navbar by omar

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
     <div>
       <div className="max-w-4xl mx-auto">
+        <Navbar></Navbar>
         <Outlet />
         <Footer />
       </div>
