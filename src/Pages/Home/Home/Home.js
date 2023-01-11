80d1e93e841cf81b5de63870ab5c5a6120e2ab0e
siam50 <siamurrahman50@gmail.com>
2023-01-11 19:44:02 +0600

    review

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LifeEasier from '../LifeEasier/LifeEasier.js';
 import LookingJobs from '../LookingJobs/LookingJobs.js';
+import Review from '../Review/Review.js';
 
 const Home = () => {
     return (
         <div>
             <LookingJobs></LookingJobs>
+            <Review></Review>
             <LifeEasier />
         </div>
     );
