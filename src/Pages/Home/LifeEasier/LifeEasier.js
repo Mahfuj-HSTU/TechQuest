babd5284f8c3f7c9e0baaa15ca44b4db8574d12e
Mahfuj-HSTU <rmahfujur16@gmail.com>
2023-01-11 17:31:13 +0600

    lifeeasier

--- src/Pages/Home/LifeEasier/LifeEasier.js
+++ src/Pages/Home/LifeEasier/LifeEasier.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 const LifeEasier = () => {
     return (
-        <div>
-            <h2>We make life easier for 600+ companies since 2020</h2>
+        <div className="hero min-h-screen ">
+            <div className="lg:flex">
+                <div className='w-3/5 text-start mr-5'>
+                    <h1 className="text-4xl font-bold">We make life easier for 600+ companies since 2015</h1>
+                    <p className="py-6">Find senior tech talent who fit your needs from our global community of over 400,000 candidates who are ready to relocate or work remotely.</p>
+                    <button className="btn btn-primary">Start hiring</button>
+                </div>
+                <div className='ml-5'>
+                    <div className='w-20 h-20 border-2'>Build Diverse Teams</div>
+                    <div className='w-20 h-20 border-2'>Vetted Tech Talent</div>
+                    <div className='w-20 h-20 border-2'>Long Term Retention</div>
+                    <div className='w-20 h-20 border-2'>Risk Free</div>
+                    <div className='w-20 h-20 border-2'>The fastest way to hire</div>
+                    <div className='w-20 h-20 border-2'>Maximize your budget</div>
+                </div>
+            </div>
         </div>
     );
 };
