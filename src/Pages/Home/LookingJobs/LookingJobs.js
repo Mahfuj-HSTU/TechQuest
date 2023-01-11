ec71955e36f1059e407e4a06b6813267d070de31
siam50 <siamurrahman50@gmail.com>
2023-01-11 11:47:45 +0600

    siam's looking job section

---
+++ src/Pages/Home/LookingJobs/LookingJobs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LookingJobs = () => {
+    return (
+        <div className=' my-12'>
+            <h1 className=' text-2xl font-semibold text-blue-400'>Looking for tech jobs in Canada, the US or Europe?</h1>
+            <button className="btn btn-outline btn-primary">Apply for jobs now</button>
+        </div>
+    );
+};
+
+export default LookingJobs;
