2840d226c0e31623c2aa239580bb4b364271d58a
siam50 <siamurrahman50@gmail.com>
2023-01-11 20:23:27 +0600

    review

--- src/Pages/Home/Review/Review.js
+++ src/Pages/Home/Review/Review.js
@@ -4,30 +4,8 @@ const Review = () => {
     return (
         <div className="carousel h-52 w-full my-16">
             <div id="slide1" className="carousel-item relative w-full">
-                <div className='grid lg:grid-cols-5 grid-cols-2'>
-                    <div className="container flex flex-col w-full max-w-lg p-5 mx-auto divide-y rounded-md divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
-                        <div className=" p-3">
-                            <div className="flex space-x-4">
-                                <div>
-                                    <img src="https://source.unsplash.com/100x100/?portrait" alt="" className="object-cover w-12 h-12 rounded-full dark:bg-gray-500" />
-                                </div>
-                                <div>
-                                    <h4 className="font-bold">Leroy Jenkins</h4>
-                                    <span className="text-xs dark:text-gray-400">2 days ago</span>
-                                </div>
-                            </div>
-                            <div className="rating rating-sm">
-                                <input type="radio" name="rating-1" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-1" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-1" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-1" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-1" className="mask mask-star-2 bg-orange-400" checked />
-                            </div>
-                        </div>
-                        <div className="p-3 space-y-2 text-sm dark:text-gray-400">
-                            <p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae.</p>
-                        </div>
-                    </div>
+                <div className='grid lg:grid-cols-4 grid-cols-2'>
+
                     <div className="container flex flex-col w-full max-w-lg p-5 mx-auto divide-y rounded-md divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
                         <div className=" p-3">
                             <div className="flex space-x-4">
@@ -127,30 +105,8 @@ const Review = () => {
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
-                <div className='grid lg:grid-cols-5 grid-cols-2'>
-                    <div className="container flex flex-col w-full max-w-lg p-5 mx-auto divide-y rounded-md divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
-                        <div className=" p-3">
-                            <div className="flex space-x-4">
-                                <div>
-                                    <img src="https://source.unsplash.com/100x100/?portrait" alt="" className="object-cover w-12 h-12 rounded-full dark:bg-gray-500" />
-                                </div>
-                                <div>
-                                    <h4 className="font-bold">Leroy Jenkins</h4>
-                                    <span className="text-xs dark:text-gray-400">2 days ago</span>
-                                </div>
-                            </div>
-                            <div className="rating rating-sm">
-                                <input type="radio" name="rating-1" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-1" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-1" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-1" className="mask mask-star-2 bg-orange-400" />
-                                <input type="radio" name="rating-1" className="mask mask-star-2 bg-orange-400" checked />
-                            </div>
-                        </div>
-                        <div className="p-3 space-y-2 text-sm dark:text-gray-400">
-                            <p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae.</p>
-                        </div>
-                    </div>
+                <div className='grid lg:grid-cols-4 grid-cols-2'>
+
                     <div className="container flex flex-col w-full max-w-lg p-5 mx-auto divide-y rounded-md divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
                         <div className=" p-3">
                             <div className="flex space-x-4">
