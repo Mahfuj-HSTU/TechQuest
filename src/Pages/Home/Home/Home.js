1afb32b4b8e0a28f449e9c3d78a369872df627f8
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2023-01-11 22:14:15 +0600

    commit by omar

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Banner from '../Banner/Banner.js';
+import Events from '../Events/Events.js';
 import LifeEasier from '../LifeEasier/LifeEasier.js';
+import LookingJobs from '../LookingJobs/LookingJobs.js';
+import WeAreSpecialist from '../WeAreSpecialist/WeAreSpecialtist.js';
 
 const Home = () => {
     return (
         <div>
-            <LifeEasier/>
+            <Banner></Banner>
+            <LookingJobs></LookingJobs>
+            <LifeEasier />
+            <Events></Events>
+            <WeAreSpecialist />
         </div>
     );
 };
