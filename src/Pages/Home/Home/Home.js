3418e7be122a4985a5119f8801ac95aa8db6f7cb
amJayem <csejayem19@gmail.com>
2023-01-11 21:04:03 +0600

    merging conflict

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,17 +1,22 @@
 import React from "react";
 import Developers from "../../../layout/Developers.js";
+import Events from "../Events/Events.js";
+import LatestNews from "../LatestNews/LatestNews.js";
 import LifeEasier from "../LifeEasier/LifeEasier.js";
+import LookingJobs from "../LookingJobs/LookingJobs.js";
+import Review from "../Review/Review.js";
+import WeAreSpecialist from "../WeAreSpecialist/WeAreSpecialist.js";
 
 const Home = () => {
   return (
     <div>
-            <LookingJobs></LookingJobs>
-            <Review></Review>
+      <LookingJobs />
+      <Review />
       <LifeEasier />
-      <Developers  />
-            <Events></Events>
-            <LatestNews/>
-            <WeAreSpecialist />
+      <Developers />
+      <Events />
+      <LatestNews />
+      <WeAreSpecialist />
     </div>
   );
 };
