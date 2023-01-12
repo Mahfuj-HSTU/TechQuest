6682d8b18026609fb974eb557421a2ec9f1a7d16
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2023-01-13 01:34:33 +0600

    commit by Omar

--- src/Pages/Home/StartHiring/StartHiring.js
+++ src/Pages/Home/StartHiring/StartHiring.js
@@ -8,26 +8,30 @@ const StartHiring = () => {
         <div className="flex flex-col items-center justify-between w-full mb-10 lg:flex-row">
           <div className="mb-16 lg:mb-0 lg:max-w-lg lg:pr-5">
             <div className="max-w-xl mb-6">
-              <div className="mr-5">
-                <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider uppercase bg-green-300 text-teal-900 rounded-full ">
-                Hire from Anywhere
+              <div className="mr-52">
+                <p style={{whiteSpace:"nowrap"}} className="inline-block px-3 py-px mb-4 text-xl font-semibold tracking-wider uppercase bg-green-300  rounded-full ">
+                  Get The Best Professionals
                 </p>
               </div>
-              <h2 className="font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none max-w-lg mb-6">
-              Hire From Anywhere
-                
+              <div style={{marginRight: "140px"}}>
+              <h2 style={{whiteSpace:"nowrap"}} className="font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none max-w-lg mb-6">
+                Hire From Anywhere
               </h2>
-              <p className="text-gray-700 text-base md:text-lg">
-              Our Global Mobility team helps with the visa process, doing all the heavy lifting. We also support fully remote hires. Or combine both by starting remotely and then relocating.
+              </div>
+              <div >
+              <p style={{textAlign:"left", marginLeft: "10px"}} className="text-gray-700 text-base md:text-lg">
+                Our Global Mobility team helps with the visa process, doing all
+                the heavy lifting. We also support fully remote hires. Or
+                combine both by starting remotely and then relocating.
               </p>
+              </div>
             </div>
             <div className="flex items-center space-x-3">
               <Link
-                href="/"
+                style={{marginLeft: "10px"}}
                 className="w-32 transition duration-300 hover:shadow-lg"
               >
-                      <button className="btn btn-primary">Start Hiring</button>
-
+                <button className="btn btn-primary">Start Hiring</button>
               </Link>
             </div>
           </div>
@@ -54,3 +58,5 @@ const StartHiring = () => {
 };
 
 export default StartHiring;
+
+
