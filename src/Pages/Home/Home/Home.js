8e6cec0ad8517f676fa8023c471d28174b87b891
zahidhasan0 <zahid.business2002@gmail.com>
2023-01-13 20:32:26 +0600

    polished the site

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Developers from "../../../layout/Developers.js";
 import Banner from "../Banner/Banner.js";
+import EmployersCategories from "../EmployersCategories/EmployersCategories.js";
 import Events from "../Events/Events.js";
+import GetCandidateForm from "../GetCandidateForm/GetCandidateForm.js";
 import LatestNews from "../LatestNews/LatestNews.js";
 import LifeEasier from "../LifeEasier/LifeEasier.js";
 import LookingJobs from "../LookingJobs/LookingJobs.js";
@@ -17,6 +19,8 @@ const Home = () => {
       <StartHiring></StartHiring>
       <Review></Review>
       <Developers />
+      <EmployersCategories />
+      <GetCandidateForm />
       <LifeEasier />
       <Events></Events>
       <LatestNews />
