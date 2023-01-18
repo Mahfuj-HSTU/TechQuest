8fcbdbe99b4d811f67c3ca5d5a9a30f86f8954d5
amJayem <csejayem19@gmail.com>
2023-01-18 16:12:59 +0600

    redux setup successfull, deleted pnpm package

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import AuthProvider from './context/AuthProvider/AuthProvider';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AuthProvider from "./context/AuthProvider/AuthProvider";
+import { Provider } from "react-redux";
+import store from "./app/store";
 
-const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Provider>
   </React.StrictMode>
 );
 
