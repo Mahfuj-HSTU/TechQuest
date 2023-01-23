12fd651b2d501f2bd64c8960ef87271c6fc7fe6e
Asraf <asraf.uddin4407@gmail.com>
2023-01-23 11:46:01 -0600

    apply job design

--- src/features/ApplyJob/ApplyJobSlice.jsx
+++ src/features/ApplyJob/ApplyJobSlice.jsx
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
-import { ServerLink } from '../../hooks/useServerLink';
+import { ServerLink } from '../../Hooks/useServerLink';
 
 // const initialApply = {
 //     apply: [
--- src/features/ApplyJob/ApplyJobView.jsx
+++ src/features/ApplyJob/ApplyJobView.jsx
@@ -13,14 +13,28 @@ const ApplyJobView = () => {
     }, [dispatch])
 
     return (
-        <div className='my-20'>
+        <div className='my-20 '>
             <h1>MY JOBS</h1>
             {isLoading && <h1>Loading..</h1>}
             {error && <h1>{error}</h1>}
             {applications && applications?.map(app => {
-                return <div> 
-                    <p>Email: {app.email} </p>
-                </div>
+                return <div className='flex '> 
+                    {/* <p>Email: {app.email} </p>
+                    <p className='text-3xl'>{app.job.jobTitle}</p> */}
+                    <div className="card w-96 bg-base-100 shadow-xl flex-1 ">
+                      <div className="card-body border-inherit m-6 rounded-lg border-8">
+                        <h2 className="card-title">
+                        {app.job.jobTitle}
+                          <div className="badge badge-secondary">{app.job.jobStatus}</div>
+                        </h2>
+                        <p>{app.job.jobDescription}</p>
+                        <div className="card-actions justify-end">
+                          <div className="badge badge-outline">Required Skill: {app.job.mustSkills}</div> 
+                          <div className="badge badge-outline">Optional Skill: {app.job.optionalSkills}</div>
+                        </div>
+                      </div>
+                    </div>
+                 </div>
             })}
         </div>
     );
