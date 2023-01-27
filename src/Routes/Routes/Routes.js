72ac05d269b06f3faf17fd19f443f1674c028b5c
Asraf <asraf.uddin4407@gmail.com>
2023-01-27 06:57:07 -0600

    added

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -3,7 +3,7 @@ import AllEmployers from "../../features/AllEmployers/AllEmployers";
 import AllJobsView from "../../features/AllJobs/AllJobsView";
 import SingleJobView from "../../features/AllJobs/SingleJobView";
 import ApplyJobView from "../../features/ApplyJob/ApplyJobView";
-import { ServerLink } from "../../hooks/useServerLink.jsx";
+import { ServerLink } from "../../Hooks/useServerLink";
 import MainLayout from "../../layout/MainLayout";
 import AboutUs from "../../Pages/AboutUs/AboutUs/AboutUs";
 import AddJobs from "../../Pages/AddJobs/AddJobs";
--- src/features/AllJobs/AllJobsSlice.jsx
+++ src/features/AllJobs/AllJobsSlice.jsx
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import { ServerLink } from "../../hooks/useServerLink";
+import { ServerLink } from "../../Hooks/useServerLink";
 
 // fake data
 // const initialJobs = {
