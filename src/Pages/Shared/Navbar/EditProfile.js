84d92afd9eac8f35fd4366321b72bf872e394b3e
siam50 <siamurrahman50@gmail.com>
2023-01-27 17:04:41 +0600

    create new component

--- src/Pages/Shared/Navbar/EditProfile.js
+++ src/Pages/Shared/Navbar/EditProfile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useContext } from 'react';
 import { FaEdit, FaPowerOff, } from "react-icons/fa"
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 
 const EditProfile = () => {
@@ -14,14 +15,14 @@ const EditProfile = () => {
 
     return (
         <li tabIndex={1}>
-            <a>
+            <Link>
                 <div className="avatar">
                     <div className="w-8 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                        <img src="https://source.unsplash.com/100x100/?portrait" />
+                        <img src="https://source.unsplash.com/100x100/?portrait" alt='' />
                     </div>
                 </div>
 
-            </a>
+            </Link>
             <ul className="relative">
                 <div className=" p-3 space-y-2 w-60 bg-slate-200 fixed top-10 right-40">
                     <div className="flex items-center p-2 space-x-4">
@@ -29,30 +30,30 @@ const EditProfile = () => {
                         <div>
                             <h2 className="text-lg font-semibold">Loreyan Jacks</h2>
                             <span className="flex items-center space-x-1">
-                                <a rel="noopener noreferrer" href="#" className="text-xs hover:underline">{user?.email}</a>
+                                <Link rel="noopener noreferrer" href="#" className="text-xs hover:underline">{user?.email}</Link>
                             </span>
                         </div>
                     </div>
                     <div className="divide-y divide-gray-400">
                         <ul className="pt-2 pb-4 space-y-1 text-sm">
                             <li>
-                                <a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
+                                <Link rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
                                     <div className=" text-gray-500">
                                         <FaEdit></FaEdit>
                                     </div>
                                     <span>Edit Profile</span>
-                                </a>
+                                </Link>
                             </li>
 
                         </ul>
                         <ul className="pt-4 pb-2 space-y-1 text-sm">
                             <li>
-                                <a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
+                                <Link rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
                                     <div className=" text-gray-500">
                                         <FaPowerOff></FaPowerOff>
                                     </div>
                                     <span onClick={handleLogOut}>Logout</span>
-                                </a>
+                                </Link>
                             </li>
                         </ul>
                     </div>
