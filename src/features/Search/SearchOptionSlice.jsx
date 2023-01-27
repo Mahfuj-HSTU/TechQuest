1f967f39c660a33f783c798d83c7bd982661b44c
zahidhasan0 <zahid.business2002@gmail.com>
2023-01-27 15:30:04 +0600

    update user

--- src/features/Search/SearchOptionSlice.jsx
+++ src/features/Search/SearchOptionSlice.jsx
@@ -1,34 +1,37 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import { ServerLink } from "../../Hooks/useServerLink";
+import { ServerLink } from "../../hooks/useServerLink";
 
-export const fetchSearch = createAsyncThunk("searchData/fetchSearch", async (searchData) => {
+export const fetchSearch = createAsyncThunk(
+  "searchData/fetchSearch",
+  async (searchData) => {
     const res = await axios.get(`${ServerLink}/search/${searchData.title}`);
     return res.data;
-})
+  }
+);
 
 export const SearchOptionSlice = createSlice({
-    name: 'search',
-    initialState: {
-        isLoading: false,
-        searchData: [],
-        error: null
-    },
-    extraReducers: builder =>{
-        builder.addCase(fetchSearch.pending, state=>{
-            state.isLoading = true;
-        })
-        builder.addCase(fetchSearch.fulfilled, (state, action)=>{
-            state.isLoading = false;
-            state.searchData = action.payload;
-            state.error = null;
-        })
-        builder.addCase(fetchSearch.rejected, (state, action)=>{
-            state.isLoading = false;
-            state.searchData = [];
-            state.error = action.error.message;
-        })
-    },
-})
+  name: "search",
+  initialState: {
+    isLoading: false,
+    searchData: [],
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchSearch.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchSearch.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.searchData = action.payload;
+      state.error = null;
+    });
+    builder.addCase(fetchSearch.rejected, (state, action) => {
+      state.isLoading = false;
+      state.searchData = [];
+      state.error = action.error.message;
+    });
+  },
+});
 
 export default SearchOptionSlice.reducer;
