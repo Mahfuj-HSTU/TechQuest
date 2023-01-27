bdf0f8a0f150162ae11eeb695119b9e23730a944
siam50 <siamurrahman50@gmail.com>
2023-01-27 12:50:53 +0600

    add profile image for jobseeker

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -107,16 +107,56 @@ const Navbar = () => {
               </li>
               <li tabIndex={0}>
                 <a>
-                  Parent
-                  <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+                  <div className="avatar">
+                    <div className="w-8 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                      <img src="https://source.unsplash.com/100x100/?portrait" />
+                    </div>
+                  </div>
+
                 </a>
-                <ul className="p-2 bg-base-100">
-                  <li className="font-semibold">
-                    <button onClick={handleLogOut} className="btn-ghost">
-                      Log Out
-                    </button>
-                  </li>
-                  <li><a>Edit Profile</a></li>
+                <ul className="relative">
+                  <div className=" p-3 space-y-2 w-60 bg-gray-900 text-gray-100 fixed top-10 right-40">
+                    <div className="flex items-center p-2 space-x-4">
+                      <img src="https://source.unsplash.com/100x100/?portrait" alt="" className="w-12 h-12 rounded-full bg-gray-500" />
+                      <div>
+                        <h2 className="text-lg font-semibold">Leroy Jenkins</h2>
+                        <span className="flex items-center space-x-1">
+                          <a rel="noopener noreferrer" href="#" className="text-xs hover:underline text-gray-400">{user?.email}</a>
+                        </span>
+                      </div>
+                    </div>
+                    <div className="divide-y divide-gray-700">
+                      <ul className="pt-2 pb-4 space-y-1 text-sm">
+                        <li>
+                          <a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
+                            <div className=" text-gray-400">
+                              <FaEdit></FaEdit>
+                            </div>
+                            <span>Edit Profile</span>
+                          </a>
+                        </li>
+
+                      </ul>
+                      <ul className="pt-4 pb-2 space-y-1 text-sm">
+                        {/* <li>
+                          <a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
+                            <div className=" text-gray-400">
+                              <FaCog></FaCog>
+                            </div>
+                            <span>Setting</span>
+                          </a>
+                        </li> */}
+                        <li>
+                          <a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
+                            <div className=" text-gray-400">
+                              <FaPowerOff></FaPowerOff>
+                            </div>
+                            <span onClick={handleLogOut}>Logout</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
                 </ul>
               </li>
             </>
