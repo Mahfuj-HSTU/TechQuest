76cd4e429a7b541c24de33d0517190b0939d8544
amJayem <csejayem19@gmail.com>
2023-01-31 18:56:37 +0600

    sign up modal updated

--- src/Pages/SignUp/SignUpModal.js
+++ src/Pages/SignUp/SignUpModal.js
@@ -1,34 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const SignUpModal = () => {
-    return (
-        // <div>
-        //     <input type="checkbox" id="my-modal-4" className="modal-toggle" />
-        //     <label htmlFor="my-modal-4" className="modal cursor-pointer">
-        //         <div className='modal-action'>
-        //             <label className="modal-box relative" htmlFor="my-modal-4">
-        //                 <Link to='/recruiter' className="btn  btn-outline btn-info w-full mb-5">Recruiter</Link>
-        //                 <Link to='/employer' className="btn  btn-outline btn-info w-full">Employer</Link>
-        //             </label>
-        //         </div>
-        //     </label>
-        // </div>
-        <div>
-            <input type="checkbox" id="sign-up-modal" className="modal-toggle" />
-            <div className="modal">
-                <div className="modal-box">
-                    <Link to='/recruiter' className="btn  btn-outline btn-info w-full mb-5">Recruiter</Link>
-                    <Link to='/jobSeeker' className="btn  btn-outline btn-info w-full">Job Seeker</Link>
-                    <div className="modal-action">
-                        <label htmlFor="sign-up-modal" className="btn">Submit</label>
-                    </div>
-                </div>
-            </div>
+  const navigate = useNavigate();
+  const handleRecruiter = () => {
+    navigate("/recruiter");
+  };
+  const handleJobSeeker = () => {
+    navigate("/jobSeeker");
+  };
+  return (
+    // <div>
+    //     <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+    //     <label htmlFor="my-modal-4" className="modal cursor-pointer">
+    //         <div className='modal-action'>
+    //             <label className="modal-box relative" htmlFor="my-modal-4">
+    //                 <Link to='/recruiter' className="btn  btn-outline btn-info w-full mb-5">Recruiter</Link>
+    //                 <Link to='/employer' className="btn  btn-outline btn-info w-full">Employer</Link>
+    //             </label>
+    //         </div>
+    //     </label>
+    // </div>
+    <div>
+      <input type="checkbox" id="sign-up-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <div className="grid grid-cols-1 gap-5">
+            <label
+              onClick={handleRecruiter}
+              htmlFor="sign-up-modal"
+              className="btn btn-outline btn-info "
+            >
+              Recruiter
+            </label>
+            <label
+              onClick={handleJobSeeker}
+              htmlFor="sign-up-modal"
+              className="btn btn-outline btn-info "
+            >
+              jobSeeker
+            </label>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignUpModal;
-
-
