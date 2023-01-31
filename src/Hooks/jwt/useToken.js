06fa088753a886f5bd5ef45e6f35261894569a60
zahidhasan0 <zahid.business2002@gmail.com>
2023-01-31 17:32:47 +0600

    fixed some error

---
+++ src/Hooks/jwt/useToken.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { ServerLink } from "../useServerLink";
+
+const useToken = (email) => {
+  const [token, setToken] = useState("");
+  console.log(email);
+  useEffect(() => {
+    fetch(`${ServerLink}/jwt?email=${email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.accessToken);
+        if (data.accessToken) {
+          localStorage.setItem("accessToken", data.accessToken);
+          setToken(data.accessToken);
+        }
+      });
+  }, [email]);
+
+  return [token];
+};
+export default useToken;
--- src/features/AllJobs/AllJobsSlice.jsx
+++ src/features/AllJobs/AllJobsSlice.jsx
@@ -2,53 +2,52 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { ServerLink } from "../../Hooks/useServerLink";
 
-export const fetchAllJobs = createAsyncThunk('jobs/fetchAllJobs', async () => {
-	const res = await axios.get(`${ServerLink}/recruiterJobPosts`);
-	return res.data;
+export const fetchAllJobs = createAsyncThunk("jobs/fetchAllJobs", async () => {
+  const res = await axios.get(`${ServerLink}/recruiterJobPosts`);
+  return res.data;
 });
 
 export const JobsSlice = createSlice({
-
-	name: 'jobs',
-	initialState: {
-		isLoading: false,
-		jobs: [],
-		isApplied: false,
-		error: null
-	},
-	extraReducers: builder => {
-		builder.addCase(fetchAllJobs.pending, state => {
-			state.isLoading = true;
-		})
-		builder.addCase(fetchAllJobs.fulfilled, (state, action) => {
-			state.isLoading = false;
-			state.jobs = action.payload;
-			state.error = null;
-		})
-		builder.addCase(fetchAllJobs.rejected, (state, action) => {
-			state.isLoading = false;
-			state.jobs = [];
-			state.error = action.error.message;
-		})
-	},
-	reducers: {
-		showJobs: (state) => state,
-		addJobs: (state, action) => {
-			state.jobs.push(action.payload);
-		},
-		updateJobs: (state, action) => {
-			const { id, title, author } = action.payload;
-			const isJobExist = state.jobs.filter((job) => job.id === id);
-			if (isJobExist) {
-				isJobExist[0].title = title;
-				isJobExist[0].author = author;
-			}
-		},
-		deleteJobs: (state, action) => {
-			const id = action.payload;
-			state.jobs = state.jobs.filter((book) => book.id !== id);
-		},
-	},
+  name: "jobs",
+  initialState: {
+    isLoading: false,
+    jobs: [],
+    isApplied: false,
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchAllJobs.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchAllJobs.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.jobs = action.payload;
+      state.error = null;
+    });
+    builder.addCase(fetchAllJobs.rejected, (state, action) => {
+      state.isLoading = false;
+      state.jobs = [];
+      state.error = action.error.message;
+    });
+  },
+  reducers: {
+    showJobs: (state) => state,
+    addJobs: (state, action) => {
+      state.jobs.push(action.payload);
+    },
+    updateJobs: (state, action) => {
+      const { id, title, author } = action.payload;
+      const isJobExist = state.jobs.filter((job) => job.id === id);
+      if (isJobExist) {
+        isJobExist[0].title = title;
+        isJobExist[0].author = author;
+      }
+    },
+    deleteJobs: (state, action) => {
+      const id = action.payload;
+      state.jobs = state.jobs.filter((book) => book.id !== id);
+    },
+  },
 });
 
 export const { showJobs, addJobs, deleteJobs, updateJobs } = JobsSlice.actions;
