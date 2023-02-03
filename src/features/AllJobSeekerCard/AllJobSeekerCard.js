8bb92e492df73283a1d4fc49c8a18f6bd8e06e06
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2023-02-03 15:32:44 +0600

    contact directly by omar

--- src/features/AllJobSeekerCard/AllJobSeekerCard.js
+++ src/features/AllJobSeekerCard/AllJobSeekerCard.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const AllJobSeekerCard = ({ jobSeeker }) => {
+const AllJobSeekerCard = ({ jobSeeker, setContact }) => {
   const {
     jobSeekerName,
     jobSeekerEmail,
@@ -12,31 +13,35 @@ const AllJobSeekerCard = ({ jobSeeker }) => {
     jobSeekerExperience,
   } = jobSeeker;
 
-//   const sendEmail = (email) =>{
-//     email.preventDefault()
-//   }
   return (
     <div>
       <div className="bg-slate-200 card w-96 shadow-xl mx-96 my-20">
         <figure className="px-10 pt-10">
-          <img
-            src={jobSeekerImage}
-            alt="Shoes"
-            className="rounded-xl"
-          />
+          <img src={jobSeekerImage} alt="" className="rounded-xl" />
         </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">{jobSeekerName}</h2>
-          <p className="font-semibold">{jobSeekerTitle}</p>
-          <p className="font-semibold">Skills : {jobSeekerSkills}</p>
-          <p>Email : {jobSeekerEmail}</p>
-          <p>Educational Institute : {jobSeekerEducationalInstitute}</p>
-          <p>Location : {jobSeekerlocation}</p>
-          <p>Experience : {jobSeekerExperience}</p>
+        <div className="card-body ">
+          <h2 className="card-title ml-28">{jobSeekerName}</h2>
+          <p className="font-semibold text-blue-500 text-left ml-24">
+            {jobSeekerTitle}
+          </p>
+          <p className="font-semibold text-start">Skills: {jobSeekerSkills}</p>
+          <p className="text-start">Email: {jobSeekerEmail}</p>
+          <p className="text-start">
+            Educational Institute: {jobSeekerEducationalInstitute}
+          </p>
+          <p className="text-start">Location: {jobSeekerlocation}</p>
+          <p className="text-left">Experience: {jobSeekerExperience}</p>
           <form action="">
-          <div className="card-actions">
-            <button className="btn btn-primary">Contact Now</button>
-          </div>
+            <div className="card-actions">
+              <label
+                onClick={() => setContact(jobSeeker)}
+                htmlFor="contact-modal"
+                className="btn btn-primary ml-24"
+                
+              >
+                Contact Now
+              </label>
+            </div>
           </form>
         </div>
       </div>
--- src/features/AllJobSeekers/AllJobSeekers.js
+++ src/features/AllJobSeekers/AllJobSeekers.js
@@ -1,26 +1,31 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import AllJobSeekerCard from '../AllJobSeekerCard/AllJobSeekerCard';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import AllJobSeekerCard from "../AllJobSeekerCard/AllJobSeekerCard";
+import ContactNow from "../AllJobSeekerCard/ContactNow";
 
 const AllJobSeekers = () => {
-    const [jobSeekers , setJobSeekers] = useState([])
-    // const [] = useState([])
-    useEffect(() => {
-        fetch('http://localhost:5000/jobSeekersCollection')
-        .then(res => res.json())
-        .then(data => setJobSeekers(data))
-    },[])
+  const [jobSeekers, setJobSeekers] = useState([]);
+  const [contact, setContact] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:5000/jobSeekersCollection")
+      .then((res) => res.json())
+      .then((data) => setJobSeekers(data));
+  }, []);
 
-    return (
-        <div>
-            {
-                jobSeekers.map( jobSeeker => <AllJobSeekerCard
-                key={jobSeeker._id}
-                jobSeeker={jobSeeker}
-                ></AllJobSeekerCard> )
-            }
-        </div>
-    );
+  return (
+    <>
+      <div>
+        {jobSeekers.map((jobSeeker) => (
+          <AllJobSeekerCard
+            key={jobSeeker._id}
+            jobSeeker={jobSeeker}
+            setContact={setContact}
+          ></AllJobSeekerCard>
+        ))}
+      </div>
+      {contact && <ContactNow contact={contact}></ContactNow>}
+    </>
+  );
 };
 
-export default AllJobSeekers; 
+export default AllJobSeekers;
