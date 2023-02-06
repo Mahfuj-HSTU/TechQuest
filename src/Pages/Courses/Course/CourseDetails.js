c5a76880e2772aa0ae1262755b09dfca416b9e3e
amJayem <csejayem19@gmail.com>
2023-02-06 23:12:14 +0600

    condition added for payment button display

--- src/Pages/Courses/Course/CourseDetails.js
+++ src/Pages/Courses/Course/CourseDetails.js
@@ -11,7 +11,7 @@ const CourseDetails = () => {
   const course = useLoaderData();
   const dispatch = useDispatch();
   const { user } = useContext(AuthContext);
-  const state = useSelector(state => state.roleReducer.role.role)
+  const role = useSelector((state) => state.roleReducer.role.role);
   // console.log(state);
 
   const { title, img, description, instructor, price, _id } = course;
@@ -24,14 +24,23 @@ const CourseDetails = () => {
       <div className="m-5 rounded-lg">
         <img className="w-full" src={img} alt="#" />
         <div className="text-start">
-          { state === 'admin' && <RemoveCourse name={title} img={img} />}
+          {role === "admin" && <RemoveCourse name={title} img={img} />}
           <h3 className="card-title my-3 text-3xl">{title}</h3>
-          <span className="bg-sky-600 rounded-md p-1 text-white">
-            Price: {price}
-          </span>
-          <Link to={`/courses/payment/${_id}`} className="bg-green-600 rounded-lg p-1 m-1 text-white">
-            Buy This Course
-          </Link>
+          {role === 'jobSeeker' && <div>
+            {price !== "0" && (
+              <span className="bg-sky-600 rounded-md p-1 text-white">
+                Price: {price}
+              </span>
+            )}
+            {price !== "0" && (
+              <Link
+                to={`/courses/payment/${_id}`}
+                className="bg-green-600 rounded-lg p-1 m-1 text-white"
+              >
+                Buy This Course
+              </Link>
+            )}
+          </div>}
           <p className="text-justify mt-3">{description}</p>
           <p className="mt-7">
             <b>Our Experienced Instructors : </b> {instructor}
