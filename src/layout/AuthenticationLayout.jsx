373d93d1e0dd78aa6cdf982c44bd64a4dd736a66
amJayem <csejayem19@gmail.com>
2023-02-07 23:35:04 +0600

    footer added

--- src/layout/AuthenticationLayout.jsx
+++ src/layout/AuthenticationLayout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
 
 const AuthenticationLayout = () => {
@@ -11,6 +12,7 @@ const AuthenticationLayout = () => {
       <div className="max-w-[1240px] mx-auto">
         <Outlet />
       </div>
+      <Footer/>
     </div>
   );
 };
