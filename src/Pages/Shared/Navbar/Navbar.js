d7471765e410a724020d65d96a9112a5a52cb4a4
amJayem <csejayem19@gmail.com>
2023-02-10 00:00:43 +0600

    updated

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -8,16 +8,16 @@ import { fetchRole } from "../../../Hooks/Role/useRoleSlice";
 import { useEffect } from "react";
 
 const Navbar = () => {
-  const { user } = useContext( AuthContext );
-  const role = useSelector( ( state ) => state.roleReducer.role.role );
+  const { user } = useContext(AuthContext);
+  const role = useSelector((state) => state.roleReducer.role.role);
   const admin = "admin";
   const recruiter = "recruiter";
   const jobSeeker = "jobSeeker";
   const dispatch = useDispatch();
 
-  useEffect( () => {
-    dispatch( fetchRole( user?.email ) );
-  }, [ dispatch, user?.email ] );
+  useEffect(() => {
+    dispatch(fetchRole(user?.email));
+  }, [dispatch, user?.email]);
 
   const menuItems = (
     <>
@@ -26,17 +26,17 @@ const Navbar = () => {
         <Link to="/all-jobs">All Jobs</Link>
         <Link to="/about">About Us</Link>
       </li>
-      { user?.email ? (
+      {user?.email ? (
         <>
-          { role === admin && (
+          {role === admin && (
             <>
               <li className="font-medium">
                 <Link to="/courses">Courses</Link>
                 <Link to="/admin/users">All Users</Link>
               </li>
             </>
-          ) }
-          { role === recruiter && (
+          )}
+          {role === recruiter && (
             <>
               <li className="font-medium">
                 <Link to="/recruiter/all-job-seekers">All Job Seekers</Link>
@@ -44,15 +44,15 @@ const Navbar = () => {
                 <Link to="/recruiter/MyJobPost">MyPost</Link>
               </li>
             </>
-          ) }
-          { role === jobSeeker && (
+          )}
+          {role === jobSeeker && (
             <>
               <li className="font-semibold">
                 <Link to="/job-seeker/my-jobs">My Jobs</Link>
                 <Link to="/courses">Courses</Link>
               </li>
             </>
-          ) }
+          )}
         </>
       ) : (
         <>
@@ -63,16 +63,16 @@ const Navbar = () => {
             <label htmlFor="sign-up-modal">Sign Up</label>
           </li>
         </>
-      ) }
+      )}
     </>
   );
 
   return (
-    <div>
-      <div className="navbar shadow-lg bg-white shadow-sky-200 fixed h-16 top-0 z-30 left-0 right-0 mx-auto backdrop-blur-lg rounded-md">
-        <div className="navbar-start">
+    <div className="navbar justify-center shadow-lg bg-white shadow-sky-200 fixed h-16 top-0 z-30 left-0 right-0 mx-auto backdrop-blur-lg rounded-md tracking-tighter">
+      <div className="">
+        <div className="">
           <div className="dropdown">
-            <label tabIndex={ 0 } className="btn btn-ghost lg:hidden">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -89,14 +89,14 @@ const Navbar = () => {
               </svg>
             </label>
             <u
-              tabIndex={ 0 }
+              tabIndex={0}
               className="menu menu-compact dropdown-content p-2 shadow bg-gray-200 rounded-box w-52"
             >
-              { menuItems }
+              {menuItems}
             </u>
           </div>
           <Link to="/" className="btn btn-ghost normal-case text-xl">
-            { " " }
+            {" "}
             <svg
               className="w-8 text-deep-purple-accent-400"
               viewBox="0 0 24 24"
@@ -117,13 +117,15 @@ const Navbar = () => {
             </span>
           </Link>
         </div>
-        <div className="navbar-end hidden lg:flex">
+        <div className="lg:mx-52 hidden lg:flex">
           <ul className="menu menu-horizontal p-0 justify-end flex-nowrap">
-            { menuItems }
+            {menuItems}
           </ul>
         </div>
-        <EditProfile></EditProfile>
-        <SignUpModal></SignUpModal>
+        <div className="lg:navbar-end">
+          <EditProfile></EditProfile>
+          <SignUpModal></SignUpModal>
+        </div>
       </div>
     </div>
   );
