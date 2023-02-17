a4f5402afe8d1515ed833f14e747ee7278565b03
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2023-02-17 18:59:37 +0600

    all job seekers update by omar

--- src/features/AllJobSeekerCard/AllJobSeekerCard.js
+++ src/features/AllJobSeekerCard/AllJobSeekerCard.js
@@ -7,42 +7,48 @@ const AllJobSeekerCard = ({ jobSeeker, setContact }) => {
     photoUrl,
     institute,
     title,
-    jobSeekerSkills,
+    skills,
     address,
     experience,
   } = jobSeeker;
 
   return (
-    <div>
-      <div className="bg-slate-200 card w-96  shadow-xl mx-28 my-20">
-        <figure className="px-10 pt-10 object-cover">
-          <img src={photoUrl} alt="" className="rounded-xl h-96" />
-        </figure>
-        <div className="card-body ">
-          <h2 className="font-bold text-2xl text-center">{name}</h2>
-          <p className="font-semibold text-blue-500  text-center">
-            {title}
-          </p>
-          <p className="font-semibold text-start">Skills: {jobSeekerSkills}</p>
-          <p className="text-start">Email: {email}</p>
-          <p className="text-start">
-            Educational Institute: {institute}
-          </p>
-          <p className="text-start">Location: {address}</p>
-          <p className="text-left">Experience: {experience}</p>
+
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 ">
+      <hr className="mb-5" />
+      <div className="grid gap-5 row-gap-8 lg:grid-cols-2">
+        <div className="flex flex-col justify-center">
+          <div className="max-w-xl mb-6">
+            <h2 className="font-bold text-2xl text-center">{name}</h2>
+            <p className="font-semibold text-blue-500  text-center mt-2">{title}</p>
+            <p className="font-semibold text-start mt-1">Skills: {skills}</p>
+            <p className="text-start mt-1">Email: {email}</p>
+            <p className="text-start mt-1">Educational Institute: {institute}</p>
+            <p className="text-start mt-1">Location: {address}</p>
+            <p className="text-left mt-1">Experience: {experience}</p>
+          </div>
+          <div className="grid gap-5 row-gap-8 sm:grid-cols-2">
           <form action="">
-            <div className="card-actions">
-              <label
-                onClick={() => setContact(jobSeeker)}
-                htmlFor="contact-modal"
-                className="btn btn-primary ml-24"
-              >
-                Contact Now
-              </label>
-            </div>
-          </form>
+                <div className="card-actions">
+                  <label
+                    onClick={() => setContact(jobSeeker)}
+                    htmlFor="contact-modal"
+                    className="btn btn-primary ml-24 mt-8"
+                  >
+                    Contact Now
+                  </label>
+                </div>
+              </form>
+            
+          </div>
+        </div>
+        <div>
+          <figure className="px-10 pt-10 object-cover">
+            <img src={photoUrl} alt="" className="rounded-xl h-96" />
+          </figure>
         </div>
       </div>
+      <hr className="mt-16" />
     </div>
   );
 };
--- src/features/AllJobSeekers/AllJobSeekers.js
+++ src/features/AllJobSeekers/AllJobSeekers.js
@@ -43,10 +43,33 @@ const AllJobSeekers = () => {
             <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
             <ul className="menu p-4 w-80 bg-base-100 text-base-content">
               <li>
-                <Link>Sidebar Item 1</Link>
-              </li>
-              <li>
-                <Link>Sidebar Item 2</Link>
+                <Link>
+                  <div className="form-control">
+                    <div className="input-group">
+                      <input
+                        type="text"
+                        placeholder="Search…"
+                        className="input input-bordered"
+                      />
+                      <button className="btn btn-square">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                </Link>
               </li>
             </ul>
           </div>
