c9c00cfb1b4c80ed4f2829bd3c914892c46998d7
amJayem <csejayem19@gmail.com>
2023-02-20 18:52:55 +0600

    console commented out

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
   const recruiter = "recruiter";
   const jobSeeker = "jobSeeker";
   const dispatch = useDispatch();
-  console.log( role )
+  // console.log( role )
 
   useEffect( () => {
     user?.email && dispatch( fetchRole( user?.email ) );
@@ -30,7 +30,7 @@ const Navbar = () => {
   //   queryFn: () => fetch( `${ ServerLink }/users/${ user?.email }` )
   //     .then( res => res.json() )
   // } )
-  console.log( details );
+  // console.log( details );
 
 
   const menuItems = (
