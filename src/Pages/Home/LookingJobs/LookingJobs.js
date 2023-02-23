c57ef3453b3d0fb92541e49194998b9b38537678
siam50 <siamurrahman50@gmail.com>
2023-02-23 11:43:08 +0600

    pull for updating

--- src/Pages/Home/LookingJobs/LookingJobs.js
+++ src/Pages/Home/LookingJobs/LookingJobs.js
@@ -16,28 +16,28 @@ const LookingJobs = () => {
       <Link to='/auth/login' className="mt-8 rounded-3xl btn btn-outline font-semibold text-accent hover:bg-accent">
         Apply for jobs now
       </Link>
-      {/* <div className="grid grid-cols-2 md:grid-cols-4 gap-3 lg:grid-cols-7 justify-items-center my-14"> */ }
+      {/* <div className="grid grid-cols-2 md:grid-cols-4 gap-3 lg:grid-cols-7 justify-items-center my-14"> */}
       <div className="lg:flex flex-col md:flex-row flex-wrap gap-4 justify-items-center justify-center my-14 hidden ">
         <div>
-          <h3 className=" font-bold text-md whitespace-nowrap">Trusted <span className="bg-neutral p-1 rounded-md ">by 600+</span> companies</h3>
+          <h3 className=" font-bold text-md whitespace-nowrap">Trusted <span className=" bg-slate-100 p-1 rounded-md ">by 600+</span> companies</h3>
         </div>
         <div className="flex justify-center">
-          <img src={ skipLogo } alt="" />
+          <img src={skipLogo} alt="" />
         </div>
         <div className="flex justify-center">
-          <img src={ booking } alt="" />
+          <img src={booking} alt="" />
         </div>
         <div className="flex justify-center">
-          <img src={ dapper } alt="" />
+          <img src={dapper} alt="" />
         </div>
         <div className="flex justify-center">
-          <img src={ deloitte } alt="" />
+          <img src={deloitte} alt="" />
         </div>
         <div className="flex justify-center">
-          <img src={ brex } alt="" />
+          <img src={brex} alt="" />
         </div>
         <div className="flex justify-center">
-          <img src={ zalando } alt="" />
+          <img src={zalando} alt="" />
         </div>
       </div>
     </div>
