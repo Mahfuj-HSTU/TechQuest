3d27ff8d99eb084bf92825967fa0451b60f948c2
Mahfuj-HSTU <rmahfujur16@gmail.com>
2023-02-27 00:59:46 +0600

    change hover effect

--- src/features/AllJobs/AllJobsView.jsx
+++ src/features/AllJobs/AllJobsView.jsx
@@ -18,8 +18,10 @@ const AllJobsView = () => {
 
   const { user } = useContext(AuthContext);
   const state = useSelector((state) => state);
-  // console.log(state);
+
   const jobs = state.jobsReducer.jobs;
+  console.log(jobs.length);
+
 
   const dispatch = useDispatch();
 
@@ -108,7 +110,7 @@ const AllJobsView = () => {
 
           <div className="w-3/6 ">
             <div className="md:col-span-1 hidden lg:inline md:inline ">
-              {courses.map(
+              {courses.slice(0, jobs.length-2).map(
                 (course) => (
                   <EnrolmentCourse
                     key={course._id}
