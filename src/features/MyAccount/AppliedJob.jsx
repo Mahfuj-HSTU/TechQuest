ce26628e564a2112ac1841a3d679c5a274bbb7d9
amJayem <csejayem19@gmail.com>
2023-03-02 11:18:54 +0600

    applied job component added

---
+++ src/features/MyAccount/AppliedJob.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyJobs from '../../Pages/MyJobs/MyJobs';
+
+const AppliedJob = () => {
+    return (
+        <div>
+            <MyJobs/>
+        </div>
+    );
+};
+
+export default AppliedJob;
